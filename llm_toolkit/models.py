2d8ea806454f2e8343c1b4a040ada468c4acf56c
MarkLee13 <1512543916@qq.com>
2024-06-29 18:01:35 +0800

    fix lint issue about new spec_results

--- llm_toolkit/models.py
+++ llm_toolkit/models.py
@@ -287,7 +287,8 @@ class GoogleModel(LLM):
   def generate_code(self,
                     prompt: prompts.Prompt,
                     response_dir: str,
-                    log_output: bool = False) -> None:
+                    log_output: bool = False,
+                    spec = False) -> None:
     """Generates code with internal LLM."""
     if not self.ai_binary:
       print(f'Error: This model requires a local AI binary: {self.ai_binary}')
